565f2df943debf6fbbbd766a1f596951af07d7a3
evanimata <evanimata@berkeley.edu>
2026-05-30 13:21:16 -0700

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,132 @@
+"""Main file to run a Pygame."""
+from __future__ import annotations
+import pygame as pg
+
+from sprites import Circle, Group, Rectangle
+
+
+class GameState:
+    """GameState class to manage the entire game."""
+    running = True
+
+    def __init__(self) -> None:
+        """Initiate Pygame constants and variables."""
+        # Press the on-button for Pygame.
+        pg.init()
+
+        # BUILD the game window.
+        self.window = Window(game=self,
+                             width=800,
+                             height=600,
+                             background_color="white",
+                             title="My Pygame",
+                            )
+
+        # Initialize the main sprite group.
+        self.sprites = Group()
+        self.create_sprites()
+
+        # DEFINE time-keeping variables.
+        self.clock = pg.time.Clock()
+        self.fps = 60
+
+    def center_all_sprites(self, activate: bool=False) -> None:
+        """Make sure all sprites are centered instead of using the top-left corner."""
+        if activate:
+            for sprite in self.sprites.sprites():
+                sprite.rect.center = (sprite.x, sprite.y)
+
+    def create_sprites(self) -> None:
+        """Make sprites and sprite groups."""
+        player = Rectangle(game=self,
+                           x=self.window.width / 2,
+                           y=self.window.height / 2,
+                           width=self.window.width / 8,
+                           height=self.window.height / 6,
+                           color="red")
+
+        circle = Circle(game=self,
+                        x=self.window.width / 2,
+                        y=self.window.height / 2,
+                        radius=self.window.width / 16,
+                        color="blue")
+        
+        self.sprites.add(player, circle)
+        self.center_all_sprites(activate=True)
+
+    def check_game_events(self) -> None:
+        """DEFINE a for loop to check all Pygame events."""
+        # Loop through all game events (e.g.: mouse clicks, key buttons, etc.).
+        keys = pg.key.get_pressed()
+        self.sprites.key_hold(keys)
+        for event in pg.event.get():
+            # Screen is resized by user.
+            if event.type == pg.VIDEORESIZE:
+                print("RESIZE.")
+
+            # Top-left corner x-button.
+            if event.type == pg.QUIT:
+                self.quit()
+
+            # Key pressing / key pressed down sensing.
+            if event.type == pg.KEYDOWN:
+                self.sprites.key_press(event)
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+
+    def play(self) -> None:
+        """Main Pygame while loop."""
+        while self.running:
+            # Set the game frames-per-second.
+            self.clock.tick(self.fps)
+
+            # Call the for loop method to check all game events.
+            self.check_game_events()
+
+            # Sprite group updates.
+            self.sprites.update()
+
+            # Window & screen updates.
+            self.window.fill(self.window.background_color)
+            self.sprites.draw(self.window.screen)
+            pg.display.update()
+
+    def quit(self) -> None:
+        """Exit the Pygame program."""
+        self.running = False
+        print("\n" + "-"*30 + "\nGAME EXITING, HAVE A GOOD DAY.")
+
+
+class Window:
+    """DEFINE the Pygame window & screen."""
+    def __init__(self, game: GameState,
+                 width: int | float,
+                 height: int | float,
+                 background_color: str | tuple | list="black",
+                 title: str="Pygame") -> None:
+        """Create a screen object."""
+        self.game = game
+
+        # Window attributes.
+        self.width = width
+        self.height = height
+        self.background_color = background_color
+
+        # BUILD the Pygame screen. DO NOT EDIT.
+        self.dimensions = (self.width, self.height)
+        self.screen = pg.display.set_mode(self.dimensions, pg.RESIZABLE)
+        pg.display.set_caption(title)
+        self.fill(self.background_color)
+
+    def fill(self, color: str | tuple | list) -> None:
+        """Update the display, fill the screen with a color."""
+        self.screen.fill(color)
+
+
+# RUN THE GAME.
+if __name__ == "__main__":
+    game = GameState()
+    try:
+        game.play()
+    except KeyboardInterrupt:
+        pass
---
+++ src/sprites.py
@@ -0,0 +1,124 @@
+"""Define all Pygame objects."""
+from __future__ import annotations
+import pygame as pg
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from main import GameState
+
+
+class Group(pg.sprite.Group):
+    """Custom Pygame Group."""
+    def __init__(self) -> None:
+        """Initialize a custom group."""
+        super().__init__()
+
+    def key_press(self, event: pg.event) -> None:
+        for sprite in self.sprites():
+            sprite.key_press(event)
+
+    def key_hold(self, keys: pg.key.ScancodeWrapper) -> None:
+        for sprite in self.sprites():
+            sprite.key_hold(keys)
+
+
+class Sprite(pg.sprite.Sprite):
+    """Custom Pygame Sprite."""
+    def __init__(self) -> None:
+        """Initialize a custom sprite."""
+        super().__init__()
+    
+    def key_press(self, event: pg.event.Event) -> None:
+        """Key pressing and sensing method."""
+
+    def key_hold(self, keys: pg.key.ScancodeWrapper) -> None:
+        """Key pressing and sensing method."""
+
+    def set_velocity(self, x_velocity: int | float, y_velocity: int | float) -> None:
+        """Set the x and y velocities of the character."""
+        self.x_velocity = x_velocity
+        self.y_velocity = y_velocity
+
+    def update(self) -> None:
+        """Make rect attribute coordinates match x and y coordinate attributes."""
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+
+class Rectangle(Sprite):
+    """Rectangle sprite."""
+    def __init__(self,
+                 game: GameState,
+                 x: int | float,
+                 y: int | float,
+                 width: int | float,
+                 height: int | float,
+                 color: str) -> None:
+        """DEFINE the Rectangle sprite."""
+        super().__init__()
+        self.game = game
+
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.dimensions = (self.width, self.height)
+        self.color = color
+
+        self.image = pg.Surface(self.dimensions, pg.SRCALPHA)
+        self.image.fill(self.color)
+        self.rect = pg.Rect(self.x, self.y, self.width, self.height)
+
+        self.set_velocity(x_velocity=5,
+                          y_velocity=5)
+
+    def key_hold(self, keys: pg.key.ScancodeWrapper) -> None:
+        """Move sprite with key presses."""
+        if keys[pg.K_LEFT]:
+            self.x -= self.x_velocity
+        if keys[pg.K_RIGHT]:
+            self.x += self.x_velocity
+        if keys[pg.K_UP]:
+            self.y -= self.y_velocity
+        if keys[pg.K_DOWN]:
+            self.y += self.y_velocity
+
+
+class Circle(Sprite):
+    """Rectangle sprite."""
+    def __init__(self,
+                 game: GameState,
+                 x: int | float,
+                 y: int | float,
+                 radius: int | float,
+                 color: str) -> None:
+        """DEFINE the Circle sprite."""
+        super().__init__()
+        self.game = game
+
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.diameter = self.radius * 2
+        self.dimensions = (self.diameter, self.diameter)
+        self.color = color
+
+        self.image = pg.Surface(self.dimensions, pg.SRCALPHA)
+        pg.draw.circle(self.image, self.color, (self.radius, self.radius), self.radius)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (self.x, self.y)
+
+        self.set_velocity(x_velocity=5,
+                          y_velocity=5)
+
+    def key_hold(self, keys: pg.key.ScancodeWrapper) -> None:
+        """Move sprite with key presses."""
+        if keys[pg.K_LEFT]:
+            self.x -= self.x_velocity*2
+        if keys[pg.K_RIGHT]:
+            self.x += self.x_velocity*2
+        if keys[pg.K_UP]:
+            self.y -= self.y_velocity*2
+        if keys[pg.K_DOWN]:
+            self.y += self.y_velocity*2
